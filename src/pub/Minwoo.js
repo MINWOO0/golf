008d5eaf9b9c0edaad3fd015ef1c295057085723
LAPTOP-P946C64H\a7875 <a01095960787@gmail.com>
2022-04-05 09:35:23 +0900

    김민우test

---
+++ src/pub/Minwoo.js
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+import "@/assets/scss/guide.css";
+
+const Minwoo = () => {
+  return (
+    <div className="Minwoo">
+     <ul>
+         <li>1</li>
+         <li>2</li>
+         <li>3</li>
+         <li>4</li>
+     </ul>
+    </div>
+  );
+};
+
+export default Minwoo;
